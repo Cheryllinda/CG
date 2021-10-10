c37a9efa395385a61f644040d63c5ced4c643d22
cheryl <cheryllinda@sohu.com>
2021-10-10 15:53:41 +0000

    test commit

---
+++ cg1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+	cout<<"How old are you?\n";
+	cin>>a;
+	cout<<"En,you are "<<a<<" years old.\n";
+	cout<<"How many students in this classroom?\n";
+	cin>>b;
+	cout<<"O,There are "<<b<<" students in this classroom.\n";
+
+	return 0;
+
+}
